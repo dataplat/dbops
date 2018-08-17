3256ef0147911053d3317f03e712b30a20f79779
nvarscar <nvarscar@gmail.com>
2018-08-17 14:13:27 -0500

    trailing spaces

--- tests/etc/bump_version.ps1
+++ tests/etc/bump_version.ps1
@@ -8,7 +8,7 @@ Write-Host "Current build $version"
 
 if ($env:gitcommitmessage -notlike "*Bumping up version*") {
     [string]$newVersion = [Version]::new($version.Major, $version.Minor, ($version.Build + 1))
-    $content = Get-Content $Path 
+    $content = Get-Content $Path
     $content | % { $_ -replace $regEx, "`$1'$newVersion'" } | Out-File $Path -Force -Encoding utf8
     $newModuleFile = Invoke-Command -ScriptBlock ([scriptblock]::Create((Get-Content $Path -Raw)))
     Write-Host "New build $($newModuleFile.ModuleVersion)"
