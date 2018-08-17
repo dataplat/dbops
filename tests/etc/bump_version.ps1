b50943ca884d0a22fd7daaaef3335dcc65a73fba
nvarscar <nvarscar@gmail.com>
2018-08-17 14:04:07 -0500

    Switching to utf8

--- tests/etc/bump_version.ps1
+++ tests/etc/bump_version.ps1
@@ -1,17 +1,15 @@
 Param (
     $Path = '.\dbops.psd1'
 )
-#$path = '.\dbops.psd1'
-
-$scriptBlock = [scriptblock]::Create((gc $path -Raw))
-$moduleFile = Invoke-Command -ScriptBlock $scriptBlock
+$moduleFile = Invoke-Command -ScriptBlock ([scriptblock]::Create((Get-Content $Path -Raw)))
 $version = [Version]$moduleFile.ModuleVersion
 $regEx = "^([\s]*ModuleVersion[\s]*\=[\s]*)\'(" + [regex]::Escape($version) + ")\'[\s]*`$"
 Write-Host "Current build $version"
 
 if ($env:gitcommitmessage -notlike "*Bumping up version*") {
     [string]$newVersion = [Version]::new($version.Major, $version.Minor, ($version.Build + 1))
-    Get-Content $Path | % { $_ -replace $regEx, "`$1'$newVersion'" } | Out-File $Path -Force
-    $manifest = Test-ModuleManifest $Path -ErrorAction Stop
-    Write-Host "New build $($manifest.Version)"
+    $content = Get-Content $Path 
+    $content | % { $_ -replace $regEx, "`$1'$newVersion'" } | Out-File $Path -Force -Encoding utf8
+    $newModuleFile = Invoke-Command -ScriptBlock ([scriptblock]::Create((Get-Content $Path -Raw)))
+    Write-Host "New build $($newModuleFile.ModuleVersion)"
 }
