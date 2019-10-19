b7096a81929aa311ce73f4b637d5383e36f6ce0a
Kirill Kravtsov <nvarscar@gmail.com>
2019-10-19 15:13:08 -0700

    Configurable variable tokens

--- internal/functions/Get-VariableToken.ps1
+++ internal/functions/Get-VariableToken.ps1
@@ -1,19 +1,20 @@
-﻿Function Get-VariableTokens {
+﻿Function Get-VariableToken {
     <#
     .SYNOPSIS
     Get a list of #{tokens} from the string
-    
+
     .DESCRIPTION
     Returns an array of tokens that matches token regex #{token}
-    
+
     .PARAMETER InputString
     String to run regex against
-    
+
     .EXAMPLE
     Get-VariableTokens '#{foo} myString #{bar}' # returns @('foo','bar')
     #>
     Param (
-        [string]$InputString
+        [string]$InputString,
+        [string]$RegexString
     )
-    [regex]::matches($InputString, "\#\{([a-zA-Z0-9.]*)\}") | ForEach-Object { $_.Groups[1].Value }
+    [regex]::matches($InputString, $RegexString) | ForEach-Object { $_.Groups[1].Value }
 }
