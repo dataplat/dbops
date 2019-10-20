b57382d180a84387720b3cfea703e810aac58884
Kirill Kravtsov <nvarscar@gmail.com>
2019-10-19 17:38:55 -0700

    adjusting parameters to be mandatory

--- functions/Set-DBODefaultSetting.ps1
+++ functions/Set-DBODefaultSetting.ps1
@@ -2,24 +2,20 @@ function Set-DBODefaultSetting {
     <#
         .SYNOPSIS
             Sets configuration entries.
-    
+
         .DESCRIPTION
             This function creates or changes configuration values.
             These can be used to provide dynamic configuration information outside the PowerShell variable system.
-    
+
         .PARAMETER Name
             Name of the configuration entry.
-    
+
         .PARAMETER Value
             The value to assign to the named configuration element.
-    
-        .PARAMETER Handler
-            A scriptblock that is executed when a value is being set.
-            Is only executed if the validation was successful (assuming there was a validation, of course)
-    
+
         .PARAMETER Append
             Adds the value to the existing configuration instead of overwriting it
-    
+
         .PARAMETER Temporary
             The setting is not persisted outside the current session.
             By default, settings will be remembered across all powershell sessions.
@@ -39,22 +35,23 @@ function Set-DBODefaultSetting {
 
         .EXAMPLE
             Set-DBODefaultSetting -Name ConnectionTimeout -Value 5 -Temporary
-        
+
             Change connection timeout setting for the current Powershell session to 5 seconds.
-    
+
         .EXAMPLE
             Set-DBODefaultSetting -Name SchemaVersionTable -Value $null
-        
+
             Change the default SchemaVersionTable setting to null, disabling the deployment logging by default
     #>
     [CmdletBinding(DefaultParameterSetName = "FullName", SupportsShouldProcess)]
     param (
+        [parameter(Mandatory)]
         [string]$Name,
+        [parameter(Mandatory)]
         [AllowNull()]
         [AllowEmptyCollection()]
         [AllowEmptyString()]
         $Value,
-        [System.Management.Automation.ScriptBlock]$Handler,
         [switch]$Append,
         [switch]$Temporary,
         [ValidateSet('CurrentUser', 'AllUsers')]
@@ -66,7 +63,7 @@ function Set-DBODefaultSetting {
             Stop-PSFFunction -Message "Setting named $Name does not exist." -EnableException $true
             return
         }
-        
+
         $newValue = $Value
         if ($append) {
             $newValue += (Get-DBODefaultSetting -Name $Name -Value)
