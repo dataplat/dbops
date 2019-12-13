9dab0b911da8bac80410936cb33fdf8048e28adf
Kirill Kravtsov <nvarscar@gmail.com>
2019-12-13 00:28:47 -0800

    adding search and ensuring casing

--- internal/functions/Install-NugetPackage.ps1
+++ internal/functions/Install-NugetPackage.ps1
@@ -12,15 +12,32 @@ function Install-NugetPackage {
         [switch]$Force,
         [string]$Api = 'https://api.nuget.org/v3'
     )
-    $packageName = $Name.ToLower()
+    $packageLowerName = $Name.ToLower()
     $Api = $Api.TrimEnd('/')
+    # Get API endpoint URLs
     $index = Invoke-WebRequest "$Api/index.json" -ErrorAction Stop
     $indexObject = $index.Content | ConvertFrom-Json
-    $baseAddress = $indexObject.resources | Where-Object { $_.'@type' -eq 'PackageBaseAddress/3.0.0' }
-    $packageVersions = Invoke-WebRequest "$($baseAddress.'@id')$packageName/index.json" -ErrorAction Stop
+
+    # search for package
+    $searchUrl = $indexObject.resources | Where-Object { $_.'@type' -eq 'SearchQueryService' } | Select-Object -First 1
+    $query = "?q=PackageId:{0}&prerelease={1}" -f $Name, (-Not $SkipPreRelease).ToString().ToLower()
+    $packageInfoResponse = Invoke-WebRequest "$($searchUrl.'@id')$query" -ErrorAction Stop
+    $packageInfoObject = $packageInfoResponse.Content | ConvertFrom-Json
+    $packageInfo = $packageInfoObject.data | Select-Object -First 1
+    if (-Not $packageInfo) {
+        Write-PSFMessage -Level Critical -Message "Package $Name was not found"
+    }
+    $packageName = $packageInfo.id
+    $packageLowerName = $packageName.ToLower()
+
+    # get package versions
+    $baseAddressUrl = $indexObject.resources | Where-Object { $_.'@type' -eq 'PackageBaseAddress/3.0.0' } | Select-Object -First 1
+    $packageVersions = Invoke-WebRequest "$($baseAddressUrl.'@id')$packageLowerName/index.json" -ErrorAction Stop
     $packageVersionsObject = $packageVersions.Content | ConvertFrom-Json
     [array]$versionList = $packageVersionsObject.Versions
     Write-PSFMessage -Level Verbose -Message "Found a total of $($versionList.Count) versions of $packageName"
+
+    # filter out the versions we don't need based on parameters
     if ($SkipPreRelease) {
         $versionList = $versionList | Where-Object { try { [version]$_ } catch { $false } }
     }
@@ -38,6 +55,8 @@ function Install-NugetPackage {
     if (-Not $selectedVersion) {
         Write-PSFMessage -Level Critical -Message "Version could not be found using current parameters" -EnableException $true
     }
+
+    # download and extract the files
     Write-PSFMessage -Level Verbose -Message "Downloading version $selectedVersion of $packageName"
     $fileName = "$packageName.$selectedVersion.nupkg"
     $scopePath = switch ($Scope) {
@@ -47,7 +66,7 @@ function Install-NugetPackage {
     $path = Join-PSFPath $scopePath PackageManagement\NuGet\Packages\ "$packageName.$selectedVersion"
     $folder = New-Item -ItemType Directory -Path $path -Force
     $packagePath = Join-PSFPath $path $fileName
-    Invoke-WebRequest "$($baseAddress.'@id')$packageName/$selectedVersion/$fileName" -OutFile $packagePath -ErrorAction Stop
+    Invoke-WebRequest "$($baseAddress.'@id')$packageLowerName/$selectedVersion/$fileName" -OutFile $packagePath -ErrorAction Stop
     Write-PSFMessage -Level Verbose -Message "Extracting $fileName to $folder"
     Expand-Archive -Path $packagePath -DestinationPath $folder -Force:$Force -ErrorAction Stop
 }
