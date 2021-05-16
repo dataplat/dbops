587462a2c1e561750052a0ab946a0835f022f4f6
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-16 03:24:53 -0700

    building files only on Windows

---
+++ build/copy_build_files.ps1
@@ -0,0 +1,6 @@
+foreach ($f in (Get-Item .\src\dbops-* -Exclude dbops-tests)) {
+    'net45', 'netstandard2.0' | ForEach-Object {
+        Write-Host "Updating $_ library: $($f.Name)"
+        Copy-Item "$($f.FullName)\bin\Release\$_\$($f.Name).dll" ".\bin\lib\$_\$($f.Name).dll"
+    }
+}
