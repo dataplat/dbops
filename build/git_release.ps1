09b59f2e69eafe45c8dc8f3c526d9c5bd96247ef
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-16 22:15:52 -0700

    no redirects

--- build/git_release.ps1
+++ build/git_release.ps1
@@ -5,6 +5,6 @@ git config --global user.email $env:git_user_email
 git config --global user.name $env:git_username
 git add $moduleFile
 git commit -m "v$version"
-git push origin HEAD:development 2>&1 > $null
-git branch release/$version 2>&1 > $null
+git push origin HEAD:development
+git branch release/$version
 git push --all origin
