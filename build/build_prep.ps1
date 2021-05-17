e0b53319645202c4c5baad18fe4faaf8c96645fd
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-17 01:26:53 -0700

    adding git fetch all

--- build/build_prep.ps1
+++ build/build_prep.ps1
@@ -1,3 +1,4 @@
 dotnet new tool-manifest
 dotnet tool install Cake.Tool --version 1.1.0
 Invoke-WebRequest https://cakebuild.net/download/bootstrapper/dotnet-tool/windows -OutFile build.ps1
+git fetch --all
