fcd06405d77fa693bbb9a1e12bc2578aa5dde091
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-17 09:48:14 -0700

    disabling explicit branch

--- build/build_prep.ps1
+++ build/build_prep.ps1
@@ -1,6 +1,6 @@
 dotnet new tool-manifest
 dotnet tool install Cake.Tool --version 1.1.0
-if ($prNo = $env:APPVEYOR_PULL_REQUEST_NUMBER) {
-    Write-Host "PR Build, creating an explicit branch"
-    git checkout -qb "pr-build-$prNo"
-}
+# if ($prNo = $env:APPVEYOR_PULL_REQUEST_NUMBER) {
+#     Write-Host "PR Build, creating an explicit branch"
+#     git checkout -qb "pr-build-$prNo"
+# }
