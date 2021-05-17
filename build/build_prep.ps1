a3d6e67556a971bccb029ea6aa4204d46687a053
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-17 01:43:22 -0700

    adding all the things

--- build/build_prep.ps1
+++ build/build_prep.ps1
@@ -3,6 +3,9 @@ dotnet tool install Cake.Tool --version 1.1.0
 Invoke-WebRequest https://cakebuild.net/download/bootstrapper/dotnet-tool/windows -OutFile build.ps1
 if ($prNo = $env:APPVEYOR_PULL_REQUEST_NUMBER) {
     Write-Host "PR Build, creating an explicit branch"
-    git fetch --all
+    git fetch --unshallow
+    git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    git fetch origin
+    git branch master origin/master
     git checkout -b "pr-build-$prNo"
 }
