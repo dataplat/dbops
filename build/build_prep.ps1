e663d5bf5990c283169dcd13b383865047c2bc46
Kirill Kravtsov <nvarscar@gmail.com>
2021-05-17 09:38:25 -0700

    new build script

--- build/build_prep.ps1
+++ build/build_prep.ps1
@@ -1,6 +1,5 @@
 dotnet new tool-manifest
 dotnet tool install Cake.Tool --version 1.1.0
-Invoke-WebRequest https://cakebuild.net/download/bootstrapper/dotnet-tool/windows -OutFile build.ps1
 if ($prNo = $env:APPVEYOR_PULL_REQUEST_NUMBER) {
     Write-Host "PR Build, creating an explicit branch"
     git checkout -qb "pr-build-$prNo"
