9114ace0f36e79bd3413f702b82d31f7782c1e55
Kirill Kravtsov <nvarscar@gmail.com>
2022-11-10 00:21:31 -0800

    separating unittests

---
+++ tests/functional.prep.ps1
@@ -0,0 +1,18 @@
+﻿# install modules
+. "$PSScriptRoot\pester.prep.ps1"
+
+# install and import libraries
+. "$PSScriptRoot\import.ps1" -Internal
+
+foreach ($type in @('Oracle', 'MySQL', 'PostgreSQL')) {
+    foreach ($lib in (Get-ExternalLibrary -Type $type)) {
+        if (-not ($ver = $lib.RequiredVersion)) {
+            $ver = $lib.MaximumVersion
+        }
+
+        $package = Install-NugetPackage -Name $lib.Name -RequiredVersion $ver -Force -Confirm:$false -Scope CurrentUser
+        foreach ($path in $lib.Path) {
+            Add-Type -Path (Join-Path (Split-Path $package.Source -Parent) $path)
+        }
+    }
+}
