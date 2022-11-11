7155ff1b5c5be5ce8bdfd20a763e0dee55e4fb78
Kirill Kravtsov <nvarscar@gmail.com>
2022-11-11 14:46:18 -0800

    unittests part 4

--- tests/common/Update-DBOConfig.Tests.ps1
+++ tests/common/Update-DBOConfig.Tests.ps1
@@ -1,78 +1,54 @@
-﻿Param (
-    [switch]$Batch
-)
-
-if ($PSScriptRoot) { $commandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", ""); $here = $PSScriptRoot }
-else { $commandName = "_ManualExecution"; $here = (Get-Item . ).FullName }
-
-if (!$Batch) {
-    # Is not a part of the global batch => import module
-    #Explicitly import the module for testing
-    Import-Module "$here\..\dbops.psd1" -Force; Get-DBOModuleFileList -Type internal | ForEach-Object { . $_.FullName }
-}
-else {
-    # Is a part of a batch, output some eye-catching happiness
-    Write-Host "Running $commandName tests" -ForegroundColor Cyan
-}
-
-
-
-$workFolder = Join-PSFPath -Normalize "$here\etc" "$commandName.Tests.dbops"
-
-$packageName = Join-Path $workFolder 'TempDeployment.zip'
-$v1scripts = Join-PSFPath -Normalize "$here\etc\sqlserver-tests\success"
-$fullConfig = Join-PSFPath -Normalize "$here\etc\tmp_full_config.json"
-$fullConfigSource = Join-PSFPath -Normalize "$here\etc\full_config.json"
-$testPassword = 'TestPassword'
-$encryptedString = $testPassword | ConvertTo-SecureString -Force -AsPlainText | ConvertTo-EncryptedString
-
-Describe "Update-DBOConfig tests" -Tag $commandName, UnitTests {
+﻿Describe "Update-DBOConfig tests" -Tag UnitTests {
     BeforeAll {
-        if ((Test-Path $workFolder) -and $workFolder -like '*.Tests.dbops') { Remove-Item $workFolder -Recurse }
-        $null = New-Item $workFolder -ItemType Directory -Force
+        $commandName = $PSCommandPath.Replace(".Tests.ps1", "").Replace($PSScriptRoot, "").Trim("/")
+        . $PSScriptRoot\fixtures.ps1 -CommandName $commandName
+
+        New-Workfolder -Force
+
+        $null = New-DBOPackage -ScriptPath (Get-SourceScript -Version 1) -Name $packageName -Build 1.0 -Force
+        $encryptedString = $securePassword | ConvertTo-EncryptedString 3>$null
         (Get-Content $fullConfigSource -Raw) -replace 'replaceMe', $encryptedString | Out-File $fullConfig -Force
-        $null = New-DBOPackage -ScriptPath $v1scripts -Name $packageName -Build 1.0 -Force -ConfigurationFile $fullConfig
+        $null = New-DBOPackage -ScriptPath (Get-SourceScript -Version 1) -Name $packageName -Build 1.0 -Force -ConfigurationFile $fullConfig
     }
     AfterAll {
-        if ((Test-Path $workFolder) -and $workFolder -like '*.Tests.dbops') { Remove-Item $workFolder -Recurse }
-        if (Test-Path $fullConfig) { Remove-Item $fullConfig }
+        Remove-Workfolder
     }
     Context "Updating single config item (config/value pairs)" {
         It "updates config item with new value" {
             Update-DBOConfig -Path $packageName -ConfigName ApplicationName -Value 'MyNewApplication'
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be 'MyNewApplication'
+            $testResults.ApplicationName | Should -Be 'MyNewApplication'
         }
         It "updates config item with null value" {
             Update-DBOConfig -Path $packageName -ConfigName ApplicationName -Value $null
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be $null
+            $testResults.ApplicationName | Should -Be $null
         }
         It "should throw when config item is not specified" {
-            { Update-DBOConfig -Path $packageName -ConfigName $null -Value '123' } | Should throw
+            { Update-DBOConfig -Path $packageName -ConfigName $null -Value '123' } | Should -Throw
         }
         It "should throw when config item does not exist" {
-            { Update-DBOConfig -Path $packageName -ConfigName NonexistingItem -Value '123' } | Should throw
+            { Update-DBOConfig -Path $packageName -ConfigName NonexistingItem -Value '123' } | Should -Throw
         }
     }
     Context "Updating config items using hashtable (values)" {
         It "updates config items with new values" {
             Update-DBOConfig -Path $packageName -Configuration @{ApplicationName = 'MyHashApplication'; Database = 'MyNewDb' }
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be 'MyHashApplication'
-            $testResults.Database | Should Be 'MyNewDb'
+            $testResults.ApplicationName | Should -Be 'MyHashApplication'
+            $testResults.Database | Should -Be 'MyNewDb'
         }
         It "updates config items with a null value" {
             Update-DBOConfig -Path $packageName -Configuration @{ApplicationName = $null; Database = $null }
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be $null
-            $testResults.Database | Should Be $null
+            $testResults.ApplicationName | Should -Be $null
+            $testResults.Database | Should -Be $null
         }
         It "should throw when config item is not specified" {
-            { Update-DBOConfig -Path $packageName -Configuration $null } | Should throw
+            { Update-DBOConfig -Path $packageName -Configuration $null } | Should -Throw
         }
         It "should throw when config item does not exist" {
-            { Update-DBOConfig -Path $packageName -Configuration @{NonexistingItem = '123' } } | Should throw
+            { Update-DBOConfig -Path $packageName -Configuration @{NonexistingItem = '123' } } | Should -Throw
         }
     }
     Context "Updating config items using DBOpsConfig and a pipeline" {
@@ -80,75 +56,75 @@ Describe "Update-DBOConfig tests" -Tag $commandName, UnitTests {
             $config = New-DBOConfig -Configuration @{ApplicationName = 'MyHashApplication'; Database = 'MyNewDb' }
             Get-DBOPackage $packageName | Update-DBOConfig -Configuration $config
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be 'MyHashApplication'
-            $testResults.Database | Should Be 'MyNewDb'
+            $testResults.ApplicationName | Should -Be 'MyHashApplication'
+            $testResults.Database | Should -Be 'MyNewDb'
         }
         It "updates config items with a null value" {
             $config = New-DBOConfig -Configuration @{ApplicationName = $null; Database = $null }
             $packageName | Update-DBOConfig -Configuration $config
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be $null
-            $testResults.Database | Should Be $null
+            $testResults.ApplicationName | Should -Be $null
+            $testResults.Database | Should -Be $null
         }
     }
     Context "Updating config items using a file template" {
         It "updates config items with an empty config file" {
-            Update-DBOConfig -Path $packageName -ConfigurationFile "$here\etc\empty_config.json"
+            Update-DBOConfig -Path $packageName -ConfigurationFile "$etcFolder\empty_config.json"
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be $null
-            $testResults.SqlInstance | Should Be $null
-            $testResults.Database | Should Be $null
-            $testResults.DeploymentMethod | Should Be $null
-            $testResults.ConnectionTimeout | Should Be $null
-            $testResults.Encrypt | Should Be $null
-            $testResults.Credential | Should Be $null
-            $testResults.Username | Should Be $null
-            $testResults.Password | Should Be $null
-            $testResults.SchemaVersionTable | Should Be $null
-            $testResults.Silent | Should Be $null
+            $testResults.ApplicationName | Should -Be $null
+            $testResults.SqlInstance | Should -Be $null
+            $testResults.Database | Should -Be $null
+            $testResults.DeploymentMethod | Should -Be $null
+            $testResults.ConnectionTimeout | Should -Be $null
+            $testResults.Encrypt | Should -Be $null
+            $testResults.Credential | Should -Be $null
+            $testResults.Username | Should -Be $null
+            $testResults.Password | Should -Be $null
+            $testResults.SchemaVersionTable | Should -Be $null
+            $testResults.Silent | Should -Be $null
             $testResults.Variables.foo | Should -Be 'bar'
             $testResults.Variables.boo | Should -Be 'far'
-            $testResults.Schema | Should Be $null
+            $testResults.Schema | Should -Be $null
         }
         It "updates config items with a proper config file" {
             Update-DBOConfig -Path $packageName -ConfigurationFile $fullConfig
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.ApplicationName | Should Be "MyTestApp"
-            $testResults.SqlInstance | Should Be "TestServer"
-            $testResults.Database | Should Be "MyTestDB"
-            $testResults.DeploymentMethod | Should Be "SingleTransaction"
-            $testResults.ConnectionTimeout | Should Be 40
-            $testResults.Encrypt | Should Be $null
-            $testResults.Credential.UserName | Should Be "CredentialUser"
-            $testResults.Credential.GetNetworkCredential().Password | Should Be "TestPassword"
-            $testResults.Username | Should Be "TestUser"
-            [PSCredential]::new('test', $testResults.Password).GetNetworkCredential().Password | Should Be "TestPassword"
-            $testResults.SchemaVersionTable | Should Be "test.Table"
-            $testResults.Silent | Should Be $true
+            $testResults.ApplicationName | Should -Be "MyTestApp"
+            $testResults.SqlInstance | Should -Be "TestServer"
+            $testResults.Database | Should -Be "MyTestDB"
+            $testResults.DeploymentMethod | Should -Be "SingleTransaction"
+            $testResults.ConnectionTimeout | Should -Be 40
+            $testResults.Encrypt | Should -Be $null
+            $testResults.Credential.UserName | Should -Be "CredentialUser"
+            $testResults.Credential.GetNetworkCredential().Password | Should -Be "TestPassword"
+            $testResults.Username | Should -Be "TestUser"
+            [PSCredential]::new('test', $testResults.Password).GetNetworkCredential().Password | Should -Be "TestPassword"
+            $testResults.SchemaVersionTable | Should -Be "test.Table"
+            $testResults.Silent | Should -Be $true
             $testResults.Variables.foo | Should -Be 'bar'
             $testResults.Variables.boo | Should -Be 'far'
-            $testResults.Schema | Should Be 'testschema'
+            $testResults.Schema | Should -Be 'testschema'
         }
         It "should throw when config file is not specified" {
-            { Update-DBOConfig -Path $packageName -ConfigurationFile $null } | Should throw
+            { Update-DBOConfig -Path $packageName -ConfigurationFile $null } | Should -Throw
         }
         It "should throw when config items are wrong in the file" {
-            { Update-DBOConfig -Path $packageName -ConfigurationFile "$here\etc\wrong_config.json" } | Should throw
+            { Update-DBOConfig -Path $packageName -ConfigurationFile "$etcFolder\wrong_config.json" } | Should -Throw
         }
         It "should throw when config file does not exist" {
-            { Update-DBOConfig -Path $packageName -ConfigurationFile "$here\etc\nonexistingconfig.json" } | Should throw
+            { Update-DBOConfig -Path $packageName -ConfigurationFile "$etcFolder\nonexistingconfig.json" } | Should -Throw
         }
     }
     Context "Updating variables" {
         It "updates config variables with new hashtable" {
             Update-DBOConfig -Path $packageName -Variables @{foo = 'bar' }
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.Variables.foo | Should Be 'bar'
+            $testResults.Variables.foo | Should -Be 'bar'
         }
         It "overrides specified config with a value from -Variables" {
             Update-DBOConfig -Path $packageName -Configuration @{Variables = @{ foo = 'bar' } } -Variables @{foo = 'bar2' }
             $testResults = (Get-DBOPackage -Path $packageName).Configuration
-            $testResults.Variables.foo | Should Be 'bar2'
+            $testResults.Variables.foo | Should -Be 'bar2'
         }
     }
 }
--- tests/common/Update-DBOPackage.Tests.ps1
+++ tests/common/Update-DBOPackage.Tests.ps1
@@ -1,88 +1,66 @@
-﻿Param (
-    [switch]$Batch
-)
-
-if ($PSScriptRoot) { $commandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", ""); $here = $PSScriptRoot }
-else { $commandName = "_ManualExecution"; $here = (Get-Item . ).FullName }
-
-if (!$Batch) {
-    # Is not a part of the global batch => import module
-    #Explicitly import the module for testing
-    Import-Module "$here\..\dbops.psd1" -Force; Get-DBOModuleFileList -Type internal | ForEach-Object { . $_.FullName }
-}
-else {
-    # Is a part of a batch, output some eye-catching happiness
-    Write-Host "Running $commandName tests" -ForegroundColor Cyan
-}
-
-
-
-$workFolder = Join-PSFPath -Normalize "$here\etc" "$commandName.Tests.dbops"
-
-$packageName = Join-Path $workFolder 'TempDeployment.zip'
-$v1scripts = Join-PSFPath -Normalize "$here\etc\sqlserver-tests\success\1.sql"
-$v2scripts = Join-PSFPath -Normalize "$here\etc\sqlserver-tests\success\2.sql"
-
-Describe "Update-DBOPackage tests" -Tag $commandName, UnitTests {
+﻿Describe "Update-DBOPackage tests" -Tag UnitTests {
     BeforeAll {
-        if ((Test-Path $workFolder) -and $workFolder -like '*.Tests.dbops') { Remove-Item $workFolder -Recurse }
-        $null = New-Item $workFolder -ItemType Directory -Force
-        $null = New-DBOPackage -ScriptPath $v1scripts -Name $packageName -Build 1.0 -Force -Slim
+        $commandName = $PSCommandPath.Replace(".Tests.ps1", "").Replace($PSScriptRoot, "").Trim("/")
+        . $PSScriptRoot\fixtures.ps1 -CommandName $commandName
+
+        New-Workfolder -Force
+
+        $null = New-DBOPackage -ScriptPath (Get-SourceScript -Version 1) -Name $packageName -Build 1.0 -Force -Slim
     }
     AfterAll {
-        if ((Test-Path $workFolder) -and $workFolder -like '*.Tests.dbops') { Remove-Item $workFolder -Recurse }
+        Remove-Workfolder
     }
     Context "Updating regular package" {
         It "updates prescripts" {
             $testResults = Get-DBOPackage -Path $packageName
             $testResults.PreScripts.Scripts.FullName | Should -BeNullOrEmpty
-            Update-DBOPackage -Path $packageName -PreScriptPath $v2scripts
+            Update-DBOPackage -Path $packageName -PreScriptPath (Get-SourceScript -Version 2)
             $testResults = Get-DBOPackage -Path $packageName
-            $testResults.PreScripts.Scripts.FullName | Should Be $script2
-            $testResults.PreScripts.Scripts.PackagePath | Should Be (Get-Item $v2scripts).Name
+            $testResults.PreScripts.Scripts.FullName | Should -Be $script2
+            $testResults.PreScripts.Scripts.PackagePath | Should -Be (Split-Path (Get-SourceScript -Version 2) -Leaf)
             $testResults = Get-ArchiveItem $packageName
-            Join-PSFPath -Normalize 'content\1.0\1.sql' | Should BeIn $testResults.Path
-            Join-PSFPath -Normalize 'content\.dbops.prescripts\2.sql' | Should BeIn $testResults.Path
+            Join-PSFPath -Normalize 'content\1.0\1.sql' | Should -BeIn $testResults.Path
+            Join-PSFPath -Normalize 'content\.dbops.prescripts\2.sql' | Should -BeIn $testResults.Path
         }
         It "updates postscripts" {
             $testResults = Get-DBOPackage -Path $packageName
             $testResults.PostScripts.Scripts.FullName | Should -BeNullOrEmpty
-            Update-DBOPackage -Path $packageName -PostScriptPath $v2scripts
+            Update-DBOPackage -Path $packageName -PostScriptPath (Get-SourceScript -Version 2)
             $testResults = Get-DBOPackage -Path $packageName
-            $testResults.PostScripts.Scripts.FullName | Should Be $script2
-            $testResults.PostScripts.Scripts.PackagePath | Should Be (Get-Item $v2scripts).Name
+            $testResults.PostScripts.Scripts.FullName | Should -Be $script2
+            $testResults.PostScripts.Scripts.PackagePath | Should -Be (Split-Path (Get-SourceScript -Version 2) -Leaf)
             $testResults = Get-ArchiveItem $packageName
-            Join-PSFPath -Normalize 'content\1.0\1.sql' | Should BeIn $testResults.Path
-            Join-PSFPath -Normalize 'content\.dbops.prescripts\2.sql' | Should BeIn $testResults.Path
+            Join-PSFPath -Normalize 'content\1.0\1.sql' | Should -BeIn $testResults.Path
+            Join-PSFPath -Normalize 'content\.dbops.prescripts\2.sql' | Should -BeIn $testResults.Path
         }
         # It "updates package version" {
         #     $testResults = Get-DBOPackage -Path $packageName
-        #     $testResults.Version | Should Be "1.0"
+        #     $testResults.Version | Should -Be "1.0"
         #     Update-DBOPackage -Path $packageName -Version "13.37"
         #     $testResults = Get-DBOPackage -Path $packageName
-        #     $testResults.Version | Should Be "13.37"
+        #     $testResults.Version | Should -Be "13.37"
         # }
         It "updates package slim parameter" {
             $testResults = Get-DBOPackage -Path $packageName
-            $testResults.Slim | Should Be $true
+            $testResults.Slim | Should -Be $true
             Update-DBOPackage -Path $packageName -Slim $false
             $testResults = Get-DBOPackage -Path $packageName
-            $testResults.Slim | Should Be $false
+            $testResults.Slim | Should -Be $false
         }
     }
     Context "Negative tests" {
         It "should throw when PreScript path does not exist" {
-            { Update-DBOPackage -Path $packageName -PreScriptPath 'asduwheiruwnfelwefo\sdfpoijfdsf.sps' } | Should Throw 'The following path is not valid'
+            { Update-DBOPackage -Path $packageName -PreScriptPath 'asduwheiruwnfelwefo\sdfpoijfdsf.sps' } | Should -Throw 'The following path is not valid*'
         }
         It "should throw when PostScript path does not exist" {
-            { Update-DBOPackage -Path $packageName -PostScriptPath 'asduwheiruwnfelwefo\sdfpoijfdsf.sps' } | Should Throw 'The following path is not valid'
+            { Update-DBOPackage -Path $packageName -PostScriptPath 'asduwheiruwnfelwefo\sdfpoijfdsf.sps' } | Should -Throw 'The following path is not valid*'
         }
         It "should throw when config item does not exist" {
-            { Update-DBOPackage -Path $packageName -ConfigName NonexistingItem -Value '123' } | Should throw
+            { Update-DBOPackage -Path $packageName -ConfigName NonexistingItem -Value '123' } | Should -Throw
         }
         # It "returns error when build version is null or empty" {
-        #     { Update-DBOPackage -Name $packageName -Version $null } | Should Throw 'Version not specified'
-        #     { Update-DBOPackage -Name $packageName -Version "" } | Should Throw 'Version not specified'
+        #     { Update-DBOPackage -Name $packageName -Version $null } | Should -Throw 'Version not specified'
+        #     { Update-DBOPackage -Name $packageName -Version "" } | Should -Throw 'Version not specified'
         # }
     }
 }
