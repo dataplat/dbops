0685795c26edb042c1325089ae47859de28cb546
Kirill Kravtsov <nvarscar@gmail.com>
2022-11-11 18:44:12 -0800

    removing outdated files

--- tests/appveyor.post.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Write-Host -Object "appveyor.post: Sending coverage data" -ForeGroundColor DarkGreen
-# Push-AppveyorArtifact PesterResultsCoverage.json -FileName "PesterResultsCoverage"
-# codecov -f PesterResultsCoverage.json --flag "ps,$($env:SCENARIO.toLower())" | Out-Null
-
--- tests/pester.prep.ps1
+++ tests/pester.prep.ps1
@@ -3,8 +3,6 @@ Write-Host -ForegroundColor Cyan "Installing modules for Powershell $($PSVersion
 Write-Host -Object "pester.prep: Install Pester" -ForegroundColor DarkGreen
 Uninstall-Module -Name Pester -Force -AllVersions -ErrorAction SilentlyContinue | Out-Null
 Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -MinimumVersion 5.0 | Out-Null
-Write-Host -Object "pester.prep: Install Assert" -ForegroundColor DarkGreen
-Install-Module -Name Assert -Repository PSGallery -Force -Scope CurrentUser | Out-Null
 Write-Host -Object "pester.prep: Install PSFramework" -ForegroundColor DarkGreen
 Install-Module -Name PSFramework -Repository PSGallery -Force -Scope CurrentUser | Out-Null
 Write-Host -Object "pester.prep: Install ziphelper" -ForegroundColor DarkGreen
