c1a2dc16c58c367921a34c3a6b97f91932cc5580
Kirill Kravtsov <nvarscar@gmail.com>
2022-11-11 16:56:50 -0800

    mysql 8.0

--- tests/install_dependencies.ps1
+++ tests/install_dependencies.ps1
@@ -13,6 +13,7 @@ foreach ($t in $Type) {
 
         $package = Install-NugetPackage -Name $lib.Name -RequiredVersion $ver -Force -Confirm:$false -Scope CurrentUser
         if ($Load) {
+            Write-Host -ForegroundColor Cyan "Loading dependency $lib.Name $ver"
             foreach ($path in $lib.Path) {
                 Add-Type -Path (Join-Path (Split-Path $package.Source -Parent) $path)
             }
